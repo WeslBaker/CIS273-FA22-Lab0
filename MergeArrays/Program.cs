02d5d976159279d42aee396b5d36d2639eb717ae
WeslBaker <wesley.baker@students.fhu.edu>
2022-09-12 23:27:39 -0500

    Completed all tests

--- MergeArrays/Program.cs
+++ MergeArrays/Program.cs
@@ -5,12 +5,35 @@
     {
         public static void Main(string[] args)
         {
-            
+
         }
 
         public static int[] MergeSortedArrays(int[] array1, int[] array2)
         {
-            return new int[10];
+            List<int> list1 = array1.ToList();
+            List<int> list2 = array2.ToList();
+            List<int> mergedList = new List<int>();
+
+            foreach(int num in list1)
+            {
+                mergedList.Add(num);
+            }
+            
+            foreach(int num in list2)
+            {
+                mergedList.Add(num);
+            }
+
+            mergedList.Sort();
+            int[] mergedArray = new int[0];
+
+            foreach(int num in mergedList)
+            {
+                Array.Resize(ref mergedArray, mergedArray.Length + 1);
+                mergedArray[mergedArray.Length - 1] = num;
+            }
+            return mergedArray;
+
         }
     }
 
